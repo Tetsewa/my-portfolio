ef7bd248f7fcccbc3c4be699c59c0315a81f81f3
Tetsewa <amatwilliams@gmail.com>
2024-09-12 22:22:30 +0200

    pages structure created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#aboutme">About Me</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#technologies">Technologies</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
