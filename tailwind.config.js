34470c27fcddd21824baf8b48498cbdc251bcedf
Tetsewa <amatwilliams@gmail.com>
2024-09-14 01:13:54 +0200

    update with content type

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Adjust this path based on where your files are located
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
