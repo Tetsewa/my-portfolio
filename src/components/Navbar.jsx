8342c2608729151fbfb21a39e596cfbc96cff8bb
Tetsewa <amatwilliams@gmail.com>
2024-09-22 23:40:26 +0200

    extra styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,18 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 text-white shadow-lg">
       <div className="container mx-auto px-4 py-4 flex justify-between items-start">
-        {/* Logo Section - Top Left */}
+        
         <div className="flex items-start">
-          {/* <img 
-            src="/logo.jpg" 
+          <img 
+            src="logo.png" 
             alt="Logo" 
             className="h-12 w-12 mr-4 rounded-full"
-          /> */}
-          <span className="text-2xl font-bold">Ama Williams</span>
+          />
+          {/* <span className="text-2xl font-bold">Ama Williams</span> */}
         </div>
 
-        {/* Navigation Links - Top Right */}
+      
         <ul className="flex space-x-8 text-lg items-start">
-          <li><a href="#home" className="hover:text-blue-400">Home</a></li>
           <li><a href="#aboutme" className="hover:text-blue-400">About Me</a></li>
           <li><a href="#projects" className="hover:text-blue-400">Projects</a></li>
           <li><a href="#technologies" className="hover:text-blue-400">Technologies</a></li>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,6 @@ const projects = [
     {
         title: "Jul 2024",
         cardTitle: "BuurtHub",
-        cardSubtitle: "All-inclusive Community Forum App",
         cardDetailedText: "A full-stack community forum app where users can stay updated on local events, make posts, and trade goods and services. Technologies Used: React, Node JS, Figma, Tailwind CSS, Adaptable, Vercel, Supabase, Express, MongoDB.",
         imageUrl: "Buurthub2.png", 
         demoLink: "https://buurt-hub.vercel.app/",
@@ -13,7 +12,6 @@ const projects = [
     {
         title: "May 2024",
         cardTitle: "StyleSavvy",
-        cardSubtitle: "Beauty Services Booking App",
         cardDetailedText: "A single-page application created with React and Node for booking beauty services. Technologies Used: React, Node JS, Figma, Tailwind CSS, Netlify.",
         imageUrl: "StyleSavvy.png", 
         demoLink: "https://stylesavvyproject.netlify.app/",
@@ -22,9 +20,8 @@ const projects = [
     {
         title: "Feb 2024",
         cardTitle: "Fix It Game",
-        cardSubtitle: "Single-player Quiz Game",
-        cardDetailedText: "A quiz game testing basic knowledge of computer hardware parts. Built with HTML, CSS, and JavaScript. Technologies Used: HTML, CSS, JavaScript, Canva.",
-        imageUrl: "Fix-It-Game2.png",  
+        cardDetailedText: "A single-player quiz game testing basic knowledge of computer hardware parts. Built with HTML, CSS, and JavaScript. Technologies Used: HTML, CSS, JavaScript, Canva.",
+        imageUrl: "Fix.png",  
         demoLink: "https://tetsewa.github.io/Fix-it-Game/",
         sourceCodeLink: "https://github.com/tetsewa/fix-it-game"
     }
@@ -32,19 +29,18 @@ const projects = [
 
 const ProjectCard = ({ project }) => {
     return (
-        <div className="group w-96 h-192 [perspective:1000px]">
+        <div className="group w-96 h-80 [perspective:1000px]">
             <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
                 <div className="absolute inset-0">
-                    <img className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src={project.imageUrl} alt={project.cardTitle} />
+                    <img className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40 opacity-50" src={project.imageUrl} alt={project.cardTitle} />
                 </div>
                 <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
                     <div className="flex min-h-full flex-col items-center justify-center">
                         <h1 className="text-3xl font-bold mb-4">{project.cardTitle}</h1>
-                        <p className="text-lg mb-2">{project.cardSubtitle}</p>
                         <p className="text-base mb-4">{project.cardDetailedText}</p>
                         <div className="mt-4 flex space-x-4">
-                            <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="rounded-md bg-neutral-800 py-2 px-4 text-sm hover:bg-neutral-900">Demo</a>
-                            <a href={project.sourceCodeLink} target="_blank" rel="noopener noreferrer" className="rounded-md bg-neutral-800 py-2 px-4 text-sm hover:bg-neutral-900">Source</a>
+                            <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="rounded-md bg-white py-2 px-4 text-sm hover:bg-neutral-900">Demo</a>
+                            <a href={project.sourceCodeLink} target="_blank" rel="noopener noreferrer" className="rounded-md bg-white py-2 px-4 text-sm hover:bg-neutral-900">Source</a>
                         </div>
                     </div>
                 </div>
