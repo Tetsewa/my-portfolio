dafe55eee620b52a3d8f09dbf511be90352738c2
Tetsewa <amatwilliams@gmail.com>
2024-09-29 00:53:37 +0200

    adjusted positioning and colour of text on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,24 @@ import React from 'react';
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-black min-h-screen ">
+    <div className ="space-y-16">
       <div className="w-full">
-        {/* Subheading 1: Left-aligned */}
-        <h2 className="text-[30px] font-bold font-pacifico uppercase font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-white to-gray-800 animate-shine">
+        <h2 className="text-[30px] text-left font-bold font-pacifico font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-white to-gray-800 animate-shine">
           Hi there, I'm ...
         </h2>
       </div>
       
-      {/* Main heading: Center-aligned */}
       <div className="text-center">
-        <h1 className="text-[50px] font-bold font-pacifico uppercase font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-white to-gray-800 animate-shine">
+        <h1 className="text-[50px] font-bold font-pacifico uppercase font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-purple via-white to-gray-800 animate-shine">
           Ama Williams
         </h1>
       </div>
       
       <div className="w-full">
-        {/* Subheading 2: Right-aligned */}
-        <h2 className="text-[30px] font-bold font-pacifico uppercase font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-white to-gray-800 animate-shine">
+        <h2 className="text-[30px] font-bold font-pacifico font-mono tracking-[7px] bg-clip-text text-transparent bg-gradient-to-r from-gray-800 via-white to-gray-800 animate-shine">
           A Fullstack Developer
         </h2>
+      </div>
       </div>
     </div>
   );
